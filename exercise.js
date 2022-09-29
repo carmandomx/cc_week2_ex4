967fefde7bf890bcab1bd6bd2a55c5c1202909cd
Navidarito Chen <Navidarito.Chen@itksquare.edu.mx>
2022-09-29 11:43:12 -0400

    uploading week2 ex4

--- exercise.js
+++ exercise.js
@@ -1,8 +1,24 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const bubbleSort = (arr) => {
     /* Only make changes below this comment */
-    
-    return arr
+    // store the lenght of the array in len
+    let len = arr.length;
+    let checked;
+    //loop until is false, meaning no need to sort
+    do {
+        checked = false;
+        for (let i = 0; i < len; i++) {
+            //sort if the current value is bigger then next value
+            if (arr[i] > arr[i + 1]) {
+                let tmp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = tmp;
+                checked = true;
+            }
+        }
+    } while (checked);
+    return arr;
+
     /* Only make changes below this comment */
 }
 
