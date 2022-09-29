eb8f5fd17d7e8cdd30b5eb09fc93b27184f6e62a
LuisVazquezKsquare <luis.vazquez@itksquare.edu.mx>
2022-09-29 16:29:50 -0500

    Completed exercise 10git add exercise.js!

--- exercise.js
+++ exercise.js
@@ -1,12 +1,33 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const bubbleSort = (arr) => {
     /* Only make changes below this comment */
+    /* We need to swap adjacent elements if the first one is bigger than
+    the next one until the array is sorted */
     
-    return arr
+
+    //We declare a function that will help us do the swapping
+    const swapElements = (indexA, indexB) => {
+        let temp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = temp;
+    }
+
+    /*We go through the entire array until there are no more sorts necesary.
+    In each instance, we check every adjacent elements and check if they need to be swapped,
+    if they do, we just call the previous function*/
+    for(let i = 0; i < arr.length-1; i++){
+        for(let j = 0; j < arr.length-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swapElements(j, j+1);
+            }
+        }
+    }
+
+    //Now we just return the sorted array
+    return arr;
+    
     /* Only make changes below this comment */
 }
-
-
 
 
 
