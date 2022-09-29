34868cf6c4f2c1723b5cab196b5f6d184247e400
“Dannaé <claudia.diaz@itksquare.edu.mx>
2022-09-29 16:42:21 -0500

    Finished bubble sort exercise

--- exercise.js
+++ exercise.js
@@ -2,7 +2,18 @@
 const bubbleSort = (arr) => {
     /* Only make changes below this comment */
     
+    // using two for loops, one nested inside another
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j < arr.length+1; j++) {
+            // check by "pairs" of numbers, which one is greater, and reorganize the array
+            if (arr[i] > arr[j]) {
+                [arr[i],arr[j]]=[arr[j],arr[i]]; // swap places when the value in i > j
+            }
+        }
+        
+    }
     return arr
+    
     /* Only make changes below this comment */
 }
 
