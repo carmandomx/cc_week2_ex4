976f763d4eb28820978d106e561a7b5cd659d014
Jacinto De La Hoz1~ <Jacinto.DelaHoz@itksquare.edu.mx>
2022-09-30 10:51:26 -0400

    Fourth Ex JH

--- exercise.js
+++ exercise.js
@@ -1,12 +1,30 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const bubbleSort = (arr) => {
     /* Only make changes below this comment */
-    
+
+    for(let i = 0; i < arr.length; i++){
+       
+     
+      for(let j = 0; j < ( arr.length - i -1 ); j++){
+         
+        // Checking if the item at present iteration is greater than the next one in the iteration
+        if(arr[j] > arr[j+1]){
+           
+          // If the condition is true then swap them
+          let temp = arr[j]
+          arr[j] = arr[j + 1]
+          arr[j+1] = temp
+        }
+      }
+    }
+    // Print the sorted array
     return arr
     /* Only make changes below this comment */
 }
 
-
+   let arr = [17, 15, 22, 6, 24, 13, 28];
+        
+   console.log(bubbleSort(arr));
 
 
 
