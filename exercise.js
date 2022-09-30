8ca1a11df16c9d864e91c818f5447f7ab9d2a8a7
Luis Eduardo Fernandez Troncoso <113368001+LuisFernandez-Ksquare@users.noreply.github.com>
2022-09-30 16:59:56 -0400

    first

--- exercise.js
+++ exercise.js
@@ -1,7 +1,16 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const bubbleSort = (arr) => {
     /* Only make changes below this comment */
-    
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
     return arr
     /* Only make changes below this comment */
 }
