bb1ed1aac8a52ac7cce8ba9958dbc5bdf3faa1e9
Tomas Garcia <Tomas.Garcia@itksquare.edu.mx>
2022-09-30 12:01:05 -0500

    Task done

--- exercise.js
+++ exercise.js
@@ -2,7 +2,29 @@
 const bubbleSort = (arr) => {
     /* Only make changes below this comment */
     
-    return arr
+    let temp;
+
+    // Iterates through each item in the list
+    for (let i = 0; i < arr.length; i++) {
+
+        // This for loop makes comparisons between each element in the list
+        for (let j = 0; j < arr.length; j++) {
+            
+            // checks if the number on the left of a comparison is greater than the number on the right
+            if (arr[j] > arr[j + 1]) {
+                
+                // Swaps the numbers
+                temp = arr[j]; // Saves the arr index value to not to lose it
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+
+    }
+
+    console.log(arr);
+
+    return arr // Return the sorted arr
     /* Only make changes below this comment */
 }
 
