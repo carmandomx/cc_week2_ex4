2b4a6050398e3eece8a11148aa8add805d4c3b9b
Diego Velazquez <Diego.Velazquez@itksquare.edu.mx>
2022-09-30 14:29:42 -0500

    Solution exercise

--- exercise.js
+++ exercise.js
@@ -1,6 +1,20 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const bubbleSort = (arr) => {
     /* Only make changes below this comment */
+
+    let counter=0;
+    do{
+    counter=0;
+    for(let i=0; i<arr.length; i++){ //Here we go through the array an swap numbers if necessary
+        if(arr[i]>arr[i+1]){
+            swap=arr[i+1];
+            arr[i+1]=arr[i];
+            arr[i]=swap;
+            counter++;
+        }
+    }
+    }while(counter>0);//this do while is to do the swap cycle as many times as needed 
+
     
     return arr
     /* Only make changes below this comment */
