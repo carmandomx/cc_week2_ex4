8588939713017307262c2420a211164cbb41094e
Gabriel Osorno <Gabriel.Osorno@itksquare.edu.mx>
2022-09-30 14:20:16 -0500

    Exercise solved

--- exercise.js
+++ exercise.js
@@ -1,14 +1,27 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const bubbleSort = (arr) => {
-    /* Only make changes below this comment */
-    
-    return arr
-    /* Only make changes below this comment */
-}
+  /* Only make changes below this comment */
 
+  /* for loop that goes through arr[] */
+  for (let i = 0; i < arr.length; i++) {
+    /* for loop that goes through arr[] until arr[(length - i)] */
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      const el = arr[j];
+      const el2 = arr[j + 1];
 
+      /* if checking if el is grater than el2 to swap them */
+      if (el > el2) {
+        arr[j] = el2;
+        arr[j + 1] = el;
+      }
+    }
+  }
 
+  /* return with arr[] bubble sorted */
+  return arr;
 
+  /* Only make changes below this comment */
+};
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.bubbleSort = bubbleSort;
