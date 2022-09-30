13ea57c8a3d1271d392fe176918526f5b984eca1
Eduardo De Jesus <eduardo.dejesus@itksquare.edu.mx>
2022-09-30 16:46:13 -0400

    finished exercise

--- exercise.js
+++ exercise.js
@@ -1,7 +1,14 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const bubbleSort = (arr) => {
     /* Only make changes below this comment */
-    
+    for (let i = arr.length - 1; i > 0; i--) { //counter to go through the length of the array starting from the last one
+        for (let j = 0; j < i; j++) { //2nd counter that starts from the beginning
+          if (arr[j] > arr[j + 1]) {
+           
+            ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]] //compare then swap if the conditional is proven true
+          }
+        }
+      }
     return arr
     /* Only make changes below this comment */
 }
