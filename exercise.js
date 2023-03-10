177052c1799baf81040329e7cbb5b3e1ee31ecb7
Alexander Iván Suárez Vivas <alexander.suarez@itksquare.edu.mx>
2023-03-10 17:06:31 -0600

    Comments added

--- exercise.js
+++ exercise.js
@@ -7,14 +7,15 @@ const bubbleSort = (arr) => {
   //La segunda iteracion, j, llega hasta el penúltimo elemento del array, ya que hará una comparación con el elemento actual y el siguiente.
   for (let i = 0; i < len; i++) {
     for (let j = 0; j < len - 1; j++) {
+      //En cada iteración de j hace una comparación del elemento actual con el siguiente y si el actual es mayor que el siguiente, cambia de posición.
       if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        let temp = arr[j]; //Para el cambio de posición se crea una variable temporal que guarda el valor actual
+        arr[j] = arr[j + 1]; //Después el valor actual se cambia por el siguiente
+        arr[j + 1] = temp; //Y el siguiente se cambia por el valor de la variable temporal, que era el valor anterior.
       }
     }
   }
-  return arr;
+  return arr; //Cuando se recorra todo el array por X número de veces (siendo X el tamaño del array) se retorna el array ordenado.
   /* Only make changes below this comment */
 };
 
