8a9639126780e6faacb9645a2e6d6cf9e6d0dcdd
Alan Villanueva <alan.villanueva@itksquare.edu.mx>
2023-03-10 17:42:16 -0600

    cleaner

--- exercise.js
+++ exercise.js
@@ -1,14 +1,13 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const bubbleSort = (arr) => {
     /* Only make changes below this comment */
-    let arr_length = arr.length
-    //let sorted_until = arr.length-1 //Given that the sort method finds the largest element one by one, we use the position of the last sorted element
-    for (let i=0;i<arr_length;i++){ //we sort largest element one by one (as many iteration as elements in the array)
-        for (let j=0;j<arr_length-i;j++){ // we swap two elements until the last
-            if (arr[j]>arr[j+1]){
+    let arr_length = arr.length //length of the array
+    for (let i=0;i<arr_length;i++){ //given that we sort the largest element one by one we need as many iterations as elements in the array
+        for (let j=0;j<arr_length-i;j++){ // we swap two elements until the last sorted element (thats why we rest the number of iterations done (i) to array length)
+            if (arr[j]>arr[j+1]){ //we put the largest element between to adjacent elements on the front
                 let temp_array=arr[j]
-                arr[j]=arr[j+1]
-                arr[j+1]=temp_array
+                arr[j]=arr[j+1] 
+                arr[j+1]=temp_array // swap elements
             }
         }
     }
@@ -17,9 +16,6 @@ const bubbleSort = (arr) => {
     return arr
     /* Only make changes below this comment */
 }
-
-
-console.log(bubbleSort([504,38,1,3,5,6,7,9,23,45,123,533,43,90,75,36,65,6,7,1999]))
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
