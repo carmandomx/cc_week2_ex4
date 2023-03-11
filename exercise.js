d5c9de4dda7e193423b49b0464ff029963e9ad48
BraulioEstrada-Ksquare <Braulio.Estrada@itksquare.edu.mx>
2023-03-10 21:38:20 -0600

    ejercicio resuelto

--- exercise.js
+++ exercise.js
@@ -1,7 +1,13 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const bubbleSort = (arr) => {
     /* Only make changes below this comment */
-    
+    for(let i=0; i<arr.length;i++){ //New for and nested for for making an bubblesort algorithm
+        for(let j=0; j<arr.length-i-1;j++){
+            if(arr[j]>arr[j+1]){ //comparison between a number in the array with the next one to the right, if value on left is greater they switch places
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]]; //they will continue switching places over and over again until the array is finaly sorted
+            }
+        }
+    }
     return arr
     /* Only make changes below this comment */
 }
